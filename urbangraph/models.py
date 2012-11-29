a882573f5adc9ddeb434e81cfb991f828a524bd7
rohan <rohans@ischool.berkeley.edu>
2012-11-29 00:12:57 -0800

    updated models.py to containt tables

--- urbangraph/models.py
+++ urbangraph/models.py
@@ -5,3 +5,47 @@ class Project(models.Model):
     name = models.CharField(max_length=255, unique=True)
     title = models.CharField(max_length=255)
     description = models.TextField(validators=[validate_slug])
+	class Meta:
+		db_table = 'Project'
+
+class level(models.Model):
+    name = models.CharField(max_length=200)    
+    class Meta:
+        db_table = 'leveltb'
+    def __unicode__(self):
+        return self.name
+
+class shapeobj(models.Model):
+    levelfk = models.ForeignKey(level)
+    name = models.CharField(max_length=200)
+    parent_id = models.ForeignKey('self',null=True, blank=True)
+    class Meta:
+        db_table = 'shapetb'
+    def __unicode__(self):
+        return self.name
+        
+class runs(models.Model):
+    name = models.CharField(max_length=200)
+    class Meta:
+        db_table = 'runstb'
+
+class indicator(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.CharField(max_length=200)
+    class Meta:
+        db_table = 'indicatortb'    
+    def __unicode__(self):
+        return self.name
+    
+class indicatordata(models.Model):
+    runfk = models.ForeignKey(runs)
+    shapefk = models.ForeignKey(shapeobj)
+    indicatorfk = models.ForeignKey(indicator)
+    xvalue = models.CharField(max_length=100)
+    yvalue = models.CharField(max_length=100)
+    class Meta:
+        db_table = 'indicatordatatb'
+    def __unicode__(self):
+        return self.runfk
+    
+

a18a7e7ba6523676d8ee463e069418bc094467ff
David Greis <dgreis@ischool.berkeley.edu>
2012-12-02 22:55:56 -0800

    got graphs to work; look at index.html

--- googlegraph/main4.js
+++ googlegraph/main4.js
@@ -1,4 +1,6 @@
 var data;
+var seriesarr=[];
+var masterarray =[];
 
 $(document).ready(function() {
 
@@ -10,6 +12,9 @@ try
 google.load("visualization", "1", {packages:["corechart"]});
 google.setOnLoadCallback(drawChart);
 alert("google loaded");
+
+
+
 }
 catch(e)
 {
@@ -22,6 +27,7 @@ var fileurl = "county_table-3_2010-2040_county__county_population.tab.txt";
 
 d3.text(fileurl,function(datasetText){
 				var dataset = d3.tsv.parse(datasetText);
+        console.log("rohan look at me ",dataset);
   				for (var i = 0; i< dataset.length; i++){
   					var series = [];
   					var serobj = dataset[i];
@@ -29,17 +35,44 @@ d3.text(fileurl,function(datasetText){
   					for (var key in serobj){
   						series.push(serobj[key]);
   					}
+          console.log("I'm here again mutherfucker")
   				seriesarr.push(series);
-  				}
-			}	);
 
-data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2004',  1000,      400],
-          ['2005',  1170,      460],
-          ['2006',  660,       1120],
-          ['2007',  1030,      540]
-        ]);
+  				}
+
+          alert("file loaded");
+          var initialyear = 2010;
+console.log("seriesarr[0] length ",seriesarr.length); 
+
+for (var i=0;i<seriesarr[0].length;i++){
+  var temparr = []
+  if (i ==0) {
+      temparr[0] = "Year";
+    }
+  else{
+      temparr[0] = initialyear+'';
+      initialyear = initialyear + 2;
+
+    }
+    for (var j = 1;j<seriesarr.length;j++){
+        var temp = seriesarr[j][i];
+        if(i==0)  
+          temparr.push(temp);
+        else 
+          temparr.push(parseInt(temp));
+    }
+  masterarray.push(temparr);
+  console.log(temparr.length);
+
+}
+
+console.log(masterarray);
+console.log(masterarray.length);
+
+
+console.log("look");
+console.log(masterarray);
+data = google.visualization.arrayToDataTable(masterarray);
 //console.log(data);
 
 
@@ -47,11 +80,17 @@ data = google.visualization.arrayToDataTable([
           title: 'County Data'
         };
 var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-console.log(chart);
- chart.draw(data, options);
+chart.draw(data, options);
 alert("draw call back");
 
-}
+
+      } );
+
+
+}          
+
+
+
 
 	return false;
 });
--- js/graph.js
+++ js/graph.js
@@ -1,128 +1,97 @@
-		$(document.ready(function(){
+var data;
+var seriesarr=[];
+var masterarray =[];
 
-			alert("doc loaded");
-
-
-		}));
-		
+$(document).ready(function() {
 
 
 
-		
+alert("blah");
+try
+{
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart);
+alert("google loaded");
 
 
-			var fileurl = "../testdata/county_table-3_2010-2040_county__county_population.tab.txt";
-        	var seriesarr = []
-        	
-        	console.log(seriesarr);
-        	console.log("something");
 
-        	var w = 400,
-			h = 200,
-			margin = 20,
-			y = d3.scale.linear().domain([0, 2200000]).range([0 + margin, h - margin]),
-			x = d3.scale.linear().domain([0, 30]).range([0 + margin, w - margin]);
+}
+catch(e)
+{
+alert("bad");
+}
 
+function drawChart(){
 
-			var vis = d3.select("body")
-			    .append("svg:svg")
-			    .attr("width", w)
-			    .attr("height", h);
+var fileurl = "../testdata/county_table-3_2010-2040_county__county_population.tab.txt";
 
-
-			var g = vis.append("svg:g")
-			    .attr("transform", "translate(0, 200)");   
-        	//get data from TSV
-        	d3.text(fileurl,function(datasetText){
+d3.text(fileurl,function(datasetText){
 				var dataset = d3.tsv.parse(datasetText);
+        console.log("rohan look at me ",dataset);
   				for (var i = 0; i< dataset.length; i++){
   					var series = [];
-  					var serobj = dataset[i]
+  					var serobj = dataset[i];
   					//console.log(dataset[i])
   					for (var key in serobj){
-  						series.push(serobj[key])
+  						series.push(serobj[key]);
   					}
+          console.log("I'm here again mutherfucker")
   				seriesarr.push(series);
+
   				}
 
-  				for(var i=0;i<seriesarr.length;i++)
-        		{
-        		console.log(seriesarr[i]);
-        		drawline(seriesarr[i][0],seriesarr[i].splice(1,seriesarr[i].length));
-        		}
-  				//console.log(seriesarr);
-			});		
+          alert("file loaded");
+          var initialyear = 2010;
+console.log("seriesarr[0] length ",seriesarr.length); 
+
+for (var i=0;i<seriesarr[0].length;i++){
+  var temparr = []
+  if (i ==0) {
+      temparr[0] = "Year";
+    }
+  else{
+      temparr[0] = initialyear+'';
+      initialyear = initialyear + 2;
+
+    }
+    for (var j = 1;j<seriesarr.length;j++){
+        var temp = seriesarr[j][i];
+        if(i==0)  
+          temparr.push(temp);
+        else 
+          temparr.push(parseInt(temp));
+    }
+  masterarray.push(temparr);
+  console.log(temparr.length);
+
+}
+
+console.log(masterarray);
+console.log(masterarray.length);
 
 
-        	//var data = [3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 7];
-
-        	
-
-        	function drawline(id,data){
-
-//			,
+console.log("look");
+console.log(masterarray);
+data = google.visualization.arrayToDataTable(masterarray);
+//console.log(data);
 
 
-			
-			console.log("blah")
-			console.log(data)
-			console.log(id)
-			
-			var line = d3.svg.line()
-			    .x(function(d,i) { return x(i); })
-			    .y(function(d) { return -1 * y(d); })
-			
-			g.append("svg:path").attr("d", line(data))
-				.attr("class","line")
-				.attr("id",id);
-
-			g.append("svg:line")
-			    .attr("x1", x(0))
-			    .attr("y1", -1 * y(0))
-			    .attr("x2", x(w))
-			    .attr("y2", -1 * y(0))
+        var options = {
+          title: 'County Data'
+        };
+var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+chart.draw(data, options);
+alert("draw call back");
 
 
-			g.append("svg:line")
-			    .attr("x1", x(0))
-			    .attr("y1", -1 * y(0))
-			    .attr("x2", x(0))
-			    .attr("y2", -1 * y(d3.max(data)))
-			
-			g.selectAll(".xLabel")
-			    .data(x.ticks(5))
-			    .enter().append("svg:text")
-			    .attr("class", "xLabel")
-			    .text(String)
-			    .attr("x", function(d) { return x(d) })
-			    .attr("y", 0)
-			    .attr("text-anchor", "middle")
+      } );
 
-			g.selectAll(".yLabel")
-			    .data(y.ticks(4))
-			    .enter().append("svg:text")
-			    .attr("class", "yLabel")
-			    .text(String)
-			    .attr("x", 0)
-			    .attr("y", function(d) { return -1 * y(d) })
-			    .attr("text-anchor", "right")
-			    .attr("dy", 4)
-			
-			g.selectAll(".xTicks")
-			    .data(x.ticks(5))
-			    .enter().append("svg:line")
-			    .attr("class", "xTicks")
-			    .attr("x1", function(d) { return x(d); })
-			    .attr("y1", -1 * y(0))
-			    .attr("x2", function(d) { return x(d); })
-			    .attr("y2", -1 * y(-0.3))
 
-			g.selectAll(".yTicks")
-			    .data(y.ticks(4))
-			    .enter().append("svg:line")
-			    .attr("class", "yTicks")
-			    .attr("y1", function(d) { return -1 * y(d); })
-			    .attr("x1", x(-0.3))
-			    .attr("y2", function(d) { return -1 * y(d); })
-			    .attr("x2", x(0))
-			}
+}          
+
+
+
+
+	return false;
+});
+	
